29284814d5ca537253725aab26689877aef95308
Anthony BARNOIN <anthony.bn@outlook.fr>
2022-06-21 00:00:16 +0200

    menu sticky on mobile

--- assets/js/hamburger_menu.js
+++ assets/js/hamburger_menu.js
@@ -4,8 +4,10 @@
 	- Author:   Anthony BARNOIN
 */
 
-const hamburger = document.getElementById("menu-hamburger");
+let hamburger = document.getElementById("menu-hamburger");
 let headerWrapper = document.querySelector("header .wrapper");
+
+let navLinks = document.getElementsByClassName("nav-link");
 
 hamburger.addEventListener("change", function () {
   if (this.checked) {
@@ -13,3 +15,15 @@ hamburger.addEventListener("change", function () {
   }
   return (headerWrapper.style.display = "none");
 });
+
+/**
+ * This function close the header menu when click on one nav link (only on mobile devices).
+ */
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].onclick = () => {
+    if (hamburger.checked) {
+      hamburger.checked = false;
+      headerWrapper.style.display = "none";
+    }
+  };
+}
